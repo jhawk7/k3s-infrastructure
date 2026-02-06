0bccdfd459017c3a3d22e36a504e33e05fe76880
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2026-02-05 20:06:02 -0500

    adding argocd app and test dotnetapi

---
+++ modules/mqtt/main.tf
@@ -0,0 +1,3 @@
+// generate password file secret based on password.txt
+// generate configmap based on mosquitto.conf
+// create initialize container in yaml to mount files into config log data dirs
