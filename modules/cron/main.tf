8207f7296ae29f1716c118d6eea8f5394b51f871
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2026-02-13 23:04:12 -0500

    updating cron manifests

--- modules/cron/main.tf
+++ modules/cron/main.tf
@@ -3,19 +3,19 @@ output "kustomization_fragment" {
     secretGenerator = [ 
       {
         name = "braves-alert-secret"
-        namespace = "cron2"
+        namespace = "cron"
         envs = ["env_files/cron-braves-alert.env"]
         type = "Opaque"
       },
       {
         name = "smtp-cron-secret"
-        namespace = "cron2"
+        namespace = "cron"
         envs = ["env_files/cron-smtp.env"]
         type = "Opaque"
       },
       {
         name = "clean-dishwasher-secret"
-        namespace = "cron2"
+        namespace = "cron"
         envs = ["env_files/cron-clean-dw.env"]
         type = "Opaque"
       }
