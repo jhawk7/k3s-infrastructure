81bbdaf8cac14162dde2f292fe84fdca7ddae5c7
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2026-03-04 23:47:45 -0500

    initial helm configuration for opentel

--- modules/opentelemetry/main.tf
+++ modules/opentelemetry/main.tf
@@ -3,3 +3,57 @@
 // secret for registry creds
 // patch for load balancer ip
 // helm release
+
+locals {
+  namespace = "opentel"
+}
+
+resource "kubernetes_namespace_v1" "opentel" {
+  metadata {
+    name = local.namespace
+  }
+}
+
+resource "helm_release" "otel_collector" {
+  name       = "opentelemetry-collector"
+  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart      = "opentelemetry-collector"
+  namespace  = "opentel"
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      # mode is REQUIRED and must be 'daemonset', 'deployment', or 'statefulset'
+      mode = "deployment" 
+
+      config = {
+        receivers = {
+          otlp = {
+            protocols = {
+              grpc = { endpoint = "0.0.0.0:4317" }
+              http = { endpoint = "0.0.0.0:4318" }
+            }
+          }
+        }
+        exporters = {
+          # Default debug exporter for verifying connectivity
+          debug = {
+            verbosity = "detailed"
+          }
+        }
+        service = {
+          pipelines = {
+            traces = {
+              receivers = ["otlp"]
+              exporters = ["debug"]
+            }
+            metrics = {
+              receivers = ["otlp"]
+              exporters = ["debug"]
+            }
+          }
+        }
+      }
+    })
+  ]
+}
